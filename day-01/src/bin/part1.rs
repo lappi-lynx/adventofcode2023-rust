d38a8d471e763972814226e877704cf2b340f66b
extazystas <2153311+Extazystas@users.noreply.github.com>
2023-12-22 23:31:40 +0200

    Init setup

---
+++ day-01/src/bin/part1.rs
@@ -0,0 +1,23 @@
+fn main() {
+    println!("part1");
+    let input = include_str!("./input1.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+fn part1(input: &str) -> u64 {
+    todo()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = part1("1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet");
+        assert_eq!(142, result);
+    }}
