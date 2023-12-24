569bfb5f484f5ace7ab0ca77b6c7d181776e489a
extazystas <2153311+Extazystas@users.noreply.github.com>
2023-12-24 22:37:11 +0200

    Basic solution day#1 part1

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,16 +1,44 @@
-use miette::{Result, Error};
+use miette::{Result,Error,Context};
 // use tracing::{info, instrument};
 
-// #[tracing::instrument]
-fn main() {
+#[cfg(feature = "dhat-heap")]
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
+
+#[tracing::instrument]
+fn main() -> miette::Result<()> {
+    #[cfg(feature = "dhat-heap")]
+    let _profiler = dhat::Profiler::new_heap();
+
+    #[cfg(not(feature = "dhat-heap"))]
+    tracing_subscriber::fmt::init();
     println!("part1");
     let input = include_str!("./input1.txt");
-    let output = part1(input);
-    dbg!(output);
+    let result = process(input).context("process part 1")?;
+    dbg!(result);
+    Ok(())
 }
 
-fn part1(_input: &str) -> Result<String, Error> {
-    Ok("142".to_string())
+fn process(input: &str) -> Result<String, Error> {
+    let result = input
+    .lines()
+    .map(|line| {
+        let mut chars = line.chars()
+            .filter_map(|char| {
+                char.to_digit(10)
+            });
+        let first = chars.next().expect("should be a number");
+
+        match chars.last() {
+            Some(num) => format!("{first}{num}"),
+            None => format!("{first}{first}"),
+        }
+        .parse::<u32>()
+        .expect("should be a valid number")
+    })
+    .sum::<u32>();
+
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
@@ -23,7 +51,7 @@ mod tests {
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet";
-        let result = part1(input)?;
+        let result = process(input)?;
         assert_eq!("142", result);
         Ok(())
     }}
