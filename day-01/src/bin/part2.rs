7d26e6e993372dca139a81a2b83e59ad98055d46
extazystas <2153311+Extazystas@users.noreply.github.com>
2024-02-05 18:51:42 +0200

    Cleanup day 1 part 2 duplications

--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -1,4 +1,5 @@
-use miette::{Result,Error,Context};
+use day_01::part2::process;
+use miette::WrapErr;
 
 #[cfg(feature = "dhat-heap")]
 #[global_allocator]
@@ -17,70 +18,3 @@ fn main() -> miette::Result<()> {
     Ok(())
 }
 
-pub fn process(input: &str) -> Result<String, Error> {
-    let result = input
-        .lines()
-        .map(process_line).sum::<u32>();
-    Ok(result.to_string())
-}
-
-const SPELLED_NUMBERS: [&str; 9] = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-
-fn process_line(line: &str) -> u32 {
-    let mut result = 0;
-    let mut parsed_digits = Vec::new();
-    let chars: Vec<char> = line.chars().collect();
-    for (index, &char) in chars.iter().enumerate() {
-        if char.is_digit(10) {
-            parsed_digits.push(char.to_string())
-        }
-
-        for (i, word) in SPELLED_NUMBERS.iter().enumerate() {
-            if line[index..].starts_with(word) {
-                parsed_digits.push((i+1).to_string());
-            }
-        }
-    }
-
-    if let (Some(&ref first), Some(&ref last)) = (parsed_digits.first(), parsed_digits.last()) {
-        let concatenated = format!("{}{}", first, last);
-        result += concatenated.parse::<u32>().unwrap();
-    }
-    result
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    use rstest::rstest;
-
-    #[rstest]
-    #[case("two1nine", 29)]
-    #[case("eightwothree", 83)]
-    #[case("abcone2threexyz", 13)]
-    #[case("xtwone3four", 24)]
-    #[case("4nineeightseven2", 42)]
-    #[case("zoneight234", 14)]
-    #[case("7pqrstsixteen", 76)]
-    #[case("fivezg8jmf6hrxnhgxxttwoneg", 51)]
-    fn line_test(
-        #[case] line: &str,
-        #[case] expected: u32,
-    ) {
-        assert_eq!(expected, process_line(line))
-    }
-
-    #[test]
-    fn test_process() -> miette::Result<()> {
-        let input = "two1nine
-eightwothree
-abcone2threexyz
-xtwone3four
-4nineeightseven2
-zoneight234
-7pqrstsixteen";
-        assert_eq!("281", process(input)?);
-        Ok(())
-    }
-}
